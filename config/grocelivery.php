c9d4786c4fd25d9df88ad09b57cfc5f49de94bc3
Mateusz Lencki <mateusz.lencki@gmail.com>
2020-02-23 12:06:18 +0100

    Updated config

--- config/grocelivery.php
+++ config/grocelivery.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 return [
     'oauth_key' => [
          'api' => [
-            'url' => 'http://api.grocelivery.eu/auth/keys/public',
+            'url' => 'http://idp-webserver',
          ],
     ],
     'geolocalizer' => [
-        'host' => env('GEOLOCALIZER_HOST', 'http://api.grocelivery.eu/map'),
+        'host' => env('GEOLOCALIZER_HOST', 'http://geolocalizer-webserver'),
     ],
 ];
